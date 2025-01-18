fa9546ca4d67de61604ae7bbd010e140496e8ff8
iamlive <71422116+iamcrypto@users.noreply.github.com>
2025-01-18 19:24:26 +0530

    new database added
    ok

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -4,7 +4,7 @@ const connection = mysql.createPool({
   host: 'dbtiran.cjakccac2nys.ap-south-1.rds.amazonaws.com',
   user: 'admin',
   password: 'Ajax24x7#365',
-  database: 'win',
+  database: 'swapna',
   port: '3306' 
    
 });
